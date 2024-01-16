352688dd332e74112d02e6b41dfd8373ebc30779
NEBULA1978 <rpascualey@gmail.com>
2024-01-16 20:06:16 +0100

    Ejemplo 2 con keydown al pulsar enter muestre resultado falta borrar lo escrito en input

--- Ejejmplo2/js/index.js
+++ Ejejmplo2/js/index.js
@@ -1,9 +1,16 @@
 let nombre;
 document.querySelector(".boton").addEventListener("click",leer);
+document.querySelector("#campo").addEventListener("keydown",teclado)
 
 function leer(){
 
     nombre=document.querySelector("#campo").value;
+}
+
+function teclado(e){
+    // Cuando la tecla pulsada sea enter ira a funcion leer
+    (e.key==="Enter") && leer();
+
     // Cuando nombre no es nada(abreviado nombre)
     // (nombre!=="")
     // (nombre===true)
