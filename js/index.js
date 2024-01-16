daeeea636d7bf90875ced8cfa41194cdf64b3c01
NEBULA1978 <rpascualey@gmail.com>
2024-01-16 19:45:04 +0100

    Convierto input a Number y separo ejejmplos

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let nota;
-document.querySelector(".boton").addEventListener("click",leer);
-
-function leer(){
-    nota=document.querySelector("#campo").value;
-    document.querySelector(".caja").innerHTML=nota+1
-}
